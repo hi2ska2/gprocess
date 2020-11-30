d926255d61afd51853f6c14687578e2b84d00da8
Sung-Min Hong <smhong@gist.ac.kr>
2020-11-30 21:11:58 +0900

    The first commit

---
+++ SRC/main.cc
@@ -0,0 +1,15 @@
+// main.cc
+// Created    : 2020. 11. 30.
+// Last update: 2020. 11. 30.
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+  std::cout << "G-Process\n";
+  std::cout << "Version G-2021.02\n";
+  std::cout << "Written by\n";
+  std::cout << "Sung-Min Hong\n";
+
+  return 0;
+}
