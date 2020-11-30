0b90afcbd2b1e508019052e74db9ab5fc22e3e0e
Sung-Min Hong <smhong@gist.ac.kr>
2020-11-30 21:31:45 +0900

    Change the header

--- SRC/main.cc
+++ SRC/main.cc
@@ -6,10 +6,14 @@
 
 int main(int argc, char* argv[])
 {
-  std::cout << "G-Process\n";
-  std::cout << "Version G-2021.02\n";
-  std::cout << "Written by\n";
-  std::cout << "Sung-Min Hong\n";
+  std::cout << "****************************************************************************\n";
+  std::cout << "***                              G-Device                                ***\n";
+  std::cout << "***                          Version G-2021.02                           ***\n";
+  std::cout << "***                       Compiled on 2020. 11. 30.                      ***\n";
+  std::cout << "***                                                                      ***\n";
+  std::cout << "***                             Written by                               ***\n";
+  std::cout << "***                            Sung-Min Hong                             ***\n";
+  std::cout << "****************************************************************************\n";
 
   return 0;
 }
